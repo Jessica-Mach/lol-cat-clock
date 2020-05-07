24787e8013303a985e24c81f7e52610e4b4419cf
jessica_mach <Jessica.Mach@outlook.com>
2020-05-06 17:26:57 -0700

    Added options to select times for wake, lunch, and nap

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
-var today = new Date();
-var time = today.getHours();
-var message = " ";
-var weeHours = 2;
+// Store default values in time of day variables
 var wake = 8;
-var morning = wake + 1;
 var noon = 12;
+var lunch = noon;
 var nap = 14;
 var evening = 18;
-var party = 21;
+var weeHours = 5;
+var partyTime = 21;
 var night = 23;
+// Store default values for cat pic and message (for testing purposes)
+var message = "Code is broken.";
+var image = "cat image";	
+// Get current time
+var today = new Date();
+var time = today.getHours();
+// Connect to timeEvent and lolcat to manipulate message and cat pic
 var timeEventJS = document.getElementById("timeEvent");
 var lolcatImage = document.getElementById("lolcat");
-var image = "cat image";
+// Store value of one second as 1000 for setInterval function
 var oneSecond = 1000;
-
-var showCurrentTime = function()
-{
+// Set isPartyTime to false
+var isPartyTime = false;
+// Connect to party button and time selectors
+var partyButton = document.getElementById("partyTimeButton");
+ 
+var showCurrentTime = function(){
     // display the string on the webpage
     var clock = document.getElementById('clock');
  
@@ -56,50 +64,91 @@ var showCurrentTime = function()
 
 var updateClock = function () {
 
-	if((time >= wake) && (time < morning)){
+	if(time == wake){
 		message = "Good morning, Sunshine!";
 		image = "https://cdn.pixabay.com/photo/2017/04/04/14/29/cat-2201460_1280.jpg";
 	}
-	else if ((time >= morning) && (time < noon)){
-		message = "I'm going to need more coffee.";
-		image = "https://cdn.pixabay.com/photo/2017/08/02/14/59/cat-2571971_1280.jpg";
-	}
-	else if ((time >= noon) && (time < (noon + 1))){
+	else if (time == lunch){
 		message = "Lunch time!";
 		image = "https://cdn.pixabay.com/photo/2019/01/10/04/24/eyes-3924800_1280.jpg";
 	}
-	else if ((time >= nap) && (time < (nap + 1))){
+	else if (time == nap){
 		message = "Nap time!";
 		image = "https://cdn.pixabay.com/photo/2016/07/29/09/37/cat-1551783_1280.jpg";
+	}
+	else if (time < weeHours){
+		message = "You're awake at this hour?";
+		image = "https://cdn.pixabay.com/photo/2015/11/23/10/39/cat-1058095_1280.jpg";
+	}
+	else if ((time > weeHours) && (time < noon)){
+		message = "I'm going to need more coffee.";
+		image = "https://cdn.pixabay.com/photo/2017/08/02/14/59/cat-2571971_1280.jpg";
 	}
 	else if ((time > noon) && (time < evening)){
 		message = "What were we doing?";
 		image = "https://cdn.pixabay.com/photo/2018/01/19/15/29/cat-3092650_1280.jpg";
 	}
-	else if ((time >= evening) && (time < party)){
+	else if ((time >= evening) && (time < partyTime)){
 		message = "Good evening!";
 		image = "https://cdn.pixabay.com/photo/2018/02/21/05/17/cat-3169476_1280.jpg";
 	}
-	else if ((time >= party) && (time < night)){
+	else if (time >= partyTime && time < night){
 		message = "IZ PARTEE TIME!";
 		image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat4.jpg";
-	}
-	else if ((time >= weeHours) && (time < wake))
-	{
-		message = "You're awake at this hour?";
-		image = "https://cdn.pixabay.com/photo/2015/11/23/10/39/cat-1058095_1280.jpg";
 	}
 	else{
 		message = "Good night!";
 		image= "https://cdn.pixabay.com/photo/2016/12/30/17/27/cat-1941089_1280.jpg";
 	}
 
-	timeEventJS.innerText = message;
+	timeEventJS.innerText = message; 
 	lolcatImage.src = image;
 
 	showCurrentTime();
 };
-updateClock();
 
+var partyEvent = function() {
+   
+   if (isPartyTime === false) {
+      isPartyTime = true;
+      time = partyTime;
+      // text in the button should read "Party Over"
+      partyButton.innerText = "Party Over";
+      // color of the button should be "#0A8DAB" (bonus!)
+      partyButton.style.backgroundColor = "0A8DAB";
+   }
+   else {
+      isPartyTime = false;
+      time = new Date().getHours();
+      // text in the button should read "PARTY TIME!"
+      partyButton.innerText = "PARTY TIME!";
+      // color of the button should be "#222" (bonus!)
+      partyButton.style.backgroundColor = "#222";
+   }
+   updateClock();
+};
+
+var wakeUpEvent = function(){
+	var wakeUpTimeSelector = document.getElementById("wakeUpTimeSelector");
+	wake = wakeUpTimeSelector.value;
+
+};
+
+var lunchEvent = function(){
+	var lunchTimeSelector = document.getElementById("lunchTimeSelector");
+	lunch = lunchTimeSelector.value;
+};
+
+var napEvent = function(){
+	var napTimeSelector = document.getElementById("napTimeSelector");
+	nap = napTimeSelector.value;
+};
+
+updateClock();
+partyButton.addEventListener("click", partyEvent);
+wakeUpTimeSelector.addEventListener('change', wakeUpEvent);
+lunchTimeSelector.addEventListener('change', lunchEvent);
+napTimeSelector.addEventListener('change', napEvent);
 setInterval(updateClock, oneSecond);
+
 
